93118c40d103dd50dd1f768989b486217a40ac90
Bill Skarlatos <skarlatosbill@gmail.com>
2025-01-10 11:34:50 +0200

    clearing up main.py

--- main.py
+++ main.py
@@ -5,21 +5,20 @@ from RBFNetwork import RBF
 
 
 def main():
-
+    print(u'\u2501' * 10)
     # K-Nearest Neighbors
     print("Running K-Nearest Neighbors...")
-    KNN(1)
     KNN(3) 
-
+    print(u'\u2501' * 10)
     # Nearest Class Centroid
     print("Running Nearest Class Centroid...")
     NCC()
-
+    print(u'\u2501' * 10)
     # Hebbian Learning
     print("Running Hebbian Learning...")
     methods=["kmeans", "random", "adaptive"]
     RBF(methods[0])
-
+    print(u'\u2501' * 10)
 
 
 if __name__ == "__main__":
